ec617b1a21e74c47f7cc3cba31d172cc46f0d2b6
Konstantin-86 <131388766+Konstantin-86@users.noreply.github.com>
2023-05-22 21:44:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,81 @@
-var swiper = new Swiper(".swiper", {
+const mainBtn = document.querySelector(".item-btn");
+const formClose = document.querySelector(".form__close");
+const form = document.querySelector(".form");
+const sucsess = document.querySelector(".sucsess");
+const burger = document.querySelector(".burger");
+const list = document.querySelector(".list");
+const body = document.querySelector("body");
+const youTubeLink1 = document.querySelector(".covers__item1");
+const youTubeLink2 = document.querySelector(".covers__item2");
+const formContainer = document.querySelector(".form__container");
+
+///////////// Табы Взрослые-Дети
+const priceAdultsBtn = document.querySelector(".price-adults");
+const priceChildrenBtn = document.querySelector(".price-children");
+const priceAdultsBox = document.querySelector(".price-adults__box");
+const priceChildrenBox = document.querySelector(".price-children__box");
+priceAdultsBtn.addEventListener("click", () => {
+  priceChildrenBtn.classList.remove("active");
+  priceAdultsBtn.classList.add("active");
+  priceAdultsBox.style.display = "flex";
+  priceChildrenBox.style.display = "none";
+});
+priceChildrenBtn.addEventListener("click", () => {
+  priceChildrenBtn.classList.add("active");
+  priceAdultsBtn.classList.remove("active");
+  priceChildrenBox.style.display = "flex";
+  priceAdultsBox.style.display = "none";
+});
+////////////////// Табы форма
+const priceAdultsBoxItem = document.querySelectorAll(".price-adults__box-item");
+const formTitle = document.querySelector(".form__title");
+priceAdultsBoxItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    let title = item.querySelector("h3");
+    formTitle.textContent = `Записаться на ${title.textContent}`;
+    form.classList.add("active");
+    body.classList.add("lock");
+    formContainer.style.display = "block";
+  });
+});
+const priceChildrenBoxItem = document.querySelectorAll(
+  ".price-children__box-item"
+);
+priceChildrenBoxItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    let title = item.querySelector("h3");
+    formTitle.textContent = `Записаться на ${title.textContent}`;
+    form.classList.add("active");
+    body.classList.add("lock");
+    formContainer.style.display = "block";
+  });
+});
+//////////////Buger//////////
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  list.classList.toggle("active");
+  body.classList.toggle("lock");
+  if (burger.classList.contains("active")) {
+    mainBtn.style.display = "block";
+  } else {
+    mainBtn.style.display = "none";
+  }
+});
+//////////////Buger//////////
+
+////////////слайдер
+let swiper = new Swiper(".swiper", {
   loop: true,
   slidesPerView: 1,
+  spaceBetween: 35,
   speed: 700,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
-  pagination: {
-    el: ".swiper-pagination",
-  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
   breakpoints: {
     1220: {
       slidesPerView: 3,
@@ -20,34 +87,53 @@ var swiper = new Swiper(".swiper", {
     },
   },
 });
+//////////////Ютуб кнопки сцылки
+youTubeLink1.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "https://www.youtube.com";
+});
+youTubeLink2.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "https://www.youtube.com";
+});
 
-const mainBtn = document.querySelector(".item-btn");
-const formClose = document.querySelector(".form__close");
-const body = document.querySelector("body");
-const form = document.querySelector(".form");
-const sucsess = document.querySelector(".sucsess");
+///////главная кнопка
+
+if (burger.classList.contains("active")) {
+  mainBtn.style.display = "block";
+}
 mainBtn.addEventListener("click", () => {
   form.classList.add("active");
   body.classList.add("lock");
   mainBtn.style.display = "none";
   burger.style.display = "none";
+  list.classList.remove("active");
+  formContainer.style.display = "block";
+  formTitle.textContent = `Записаться на пробное занятие`;
 });
 formClose.addEventListener("click", () => {
   form.classList.remove("active");
   body.classList.remove("lock");
-  mainBtn.style.display = "block";
-  burger.style.display = "block";
+  burger.classList.remove("active");
+  formContainer.style.display = "none";
+  mainBtn.style.display = "none";
+  if (window.innerWidth <= 830) {
+    burger.style.display = "block";
+  }
 });
+////////вк кнопка
 
+//////////отправка в телеграмм
 const TOKEN = "6093158422:AAHT2xcxBLxjA5A74y1ryIg5wPCyHq5W9Is";
 const ChatID = "-1001928993703";
 const URI_API = `https://api.telegram.org/bot6093158422:AAHT2xcxBLxjA5A74y1ryIg5wPCyHq5W9Is/sendMessage`;
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  let message = `<b>заявка с сайта</b>\n`;
-  message += `<b>отправитель:</b> ${this.name.value}\n`;
-  message += `<b>телефон:</b> ${this.tel.value}\n`;
-  message += `<b>информация:</b> ${this.text.value}`;
+  let message = `<b>Заявка с сайта</b>\n`;
+  message += `<b>Категория:</b> ${formTitle.textContent}\n`;
+  message += `<b>Отправитель:</b> ${this.name.value}\n`;
+  message += `<b>Телефон:</b> ${this.tel.value}\n`;
+  message += `<b>Информация:</b> ${this.text.value}`;
   axios
     .post(URI_API, {
       chat_id: ChatID,
@@ -66,6 +152,7 @@ form.addEventListener("submit", function (e) {
         mainBtn.style.display = "block";
         sucsess.innerHTML = "";
         sucsess.style.display = "none";
+        formContainer.style.display = "none";
       }, 2000);
     });
 });
@@ -77,275 +164,8 @@ let phoneMask = IMask(document.getElementById("form__tel"), {
 });
 ////////////////
 
-
-
-
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
-
-gsap.from(".main-title", {
-  opacity: 0,
-  y: 20,
-  duration: 1,
-  delay: 1,
-});
-
-gsap.from(".main__subtitle", {
-  opacity: 0,
-  y: 20,
-  duration: 1,
-  delay: 1.5,
-});
-gsap.from(".menu", {
-  opacity: 0,
-  duration: 1,
-  width: 0,
-  delay: 0.5,
-});
-gsap.from(".item-btn", {
-  opacity: 0,
-  duration: 1,
-  width: 0,
-  delay: 1.5,
-});
-gsap.from(".item-btn__text", {
-  opacity: 0,
-  duration: 1,
-  delay: 2.5,
-});
-gsap.from(".list__item", {
-  opacity: 0,
-  duration: 0.8,
-  delay: 2,
-  y: -40,
-});
-gsap.from(".item-1", {
-  y: 50,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".item-1",
-    scrub: 1,
-  },
-});
-
-gsap.from(".item-11", {
-  y: 50,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".item-11",
-    scrub: 1,
-  },
-});
-gsap.from(".aboutMe__text", {
-  y: 50,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".aboutMe__text",
-    scrub: 1,
-  },
-});
-
-gsap.from(".item-2", {
-  y: 50,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".item-2",
-    scrub: 1,
-  },
-});
-gsap.from(".item-22", {
-  y: 50,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".item-22",
-    scrub: 1,
-  },
-});
-gsap.from(".item-3", {
-  y: 50,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".item-3",
-    scrub: 1,
-  },
-});
-gsap.from(".item-33", {
-  y: 50,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".item-33",
-    scrub: 1,
-  },
-});
-
-gsap.from(".aboutMe__item1", {
-  x: 100,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".aboutMe__item1",
-    scrub: 1,
-  },
-});
-gsap.from(".aboutMe__item2", {
-  x: 100,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".aboutMe__item2",
-    scrub: 1,
-  },
-});
-gsap.from(".aboutMe__item3", {
-  x: 100,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".aboutMe__item3",
-    scrub: 1,
-  },
-});
-gsap.from(".aboutMe__item4", {
-  x: 100,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".aboutMe__item4",
-    scrub: 1,
-  },
-});
-gsap.from(".swiper", {
-  y: 100,
-  opacity: 0,
-  duration: 0.8,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".swiper",
-    scrub: 1,
-  },
-});
-gsap.from(".price__item-1", {
-  y: 100,
-  opacity: 0,
-  duration: 1,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".price__item-1",
-    scrub: 1,
-  },
-});
-gsap.from(".price__item-2", {
-  y: 100,
-  opacity: 0,
-  duration: 1,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".price__item-2",
-    scrub: 1,
-  },
-});
-gsap.from(".price__item-3", {
-  y: 100,
-  opacity: 0,
-  duration: 1,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".price__item-3",
-    scrub: 1,
-  },
-});
-gsap.from(".price__item-4", {
-  y: 100,
-  opacity: 0,
-  duration: 1,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".price__item-4",
-    scrub: 1,
-  },
-});
-gsap.from(".price__item-5", {
-  y: 100,
-  opacity: 0,
-  duration: 1,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".price__item-5",
-    scrub: 1,
-  },
-});
-gsap.from(".covers__item-1", {
-  y: 100,
-  opacity: 0,
-  duration: 1,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".covers__item-1",
-    scrub: 1,
-  },
-});
-gsap.from(".covers__item-2", {
-  y: 100,
-  opacity: 0,
-  duration: 1,
-  scrollTrigger: {
-    start: "center bottom",
-    end: "+=50",
-    trigger: ".covers__item-2",
-    scrub: 1,
-  },
-});
-
-// const swiper = new Swiper(".swiper", {
-//   // effect: "cube",
-//   loop: true,
-//   autoplay: true,
-
-//   /*  pagination: {
-//     el: ".swiper-pagination",
-//   },
-
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   }, */
-// });
 
 const educationID = document.getElementById("educationID");
 const diplomaID = document.getElementById("diplomaID");
@@ -362,9 +182,6 @@ const itemLinkDiplomaID = document.querySelector(".item__link-diplomaID");
 const itemLinkCoversID = document.querySelector(".item__link-coversID");
 const itemLinkPriceID = document.querySelector(".item__link-priceID");
 const itemLinkAboutMeID = document.querySelector(".item__link-aboutMeID");
-
-const burger = document.querySelector(".burger");
-const list = document.querySelector(".list");
 
 itemlLinkeducationID.addEventListener("click", function (e) {
   e.preventDefault();
@@ -399,17 +216,7 @@ itemLinkCoversID.addEventListener("click", function (e) {
   burger.classList.remove("active");
   list.classList.remove("active");
 });
-/* itemlLinkRepertoireID.addEventListener("click", function (e) {
-  e.preventDefault();
-  body.classList.remove("lock");
 
-  gsap.to(window, {
-    duration: 1,
-    scrollTo: { y: "#repertoireID", ease: "power4" },
-  });
-  burger.classList.remove("active");
-  list.classList.remove("active");
-}); */
 itemLinkPriceID.addEventListener("click", function (e) {
   e.preventDefault();
   body.classList.remove("lock");
@@ -432,28 +239,3 @@ itemLinkAboutMeID.addEventListener("click", function (e) {
   burger.classList.remove("active");
   list.classList.remove("active");
 });
-
-//////////////Buger//////////
-(function () {
-  const burger = document.querySelector(".burger");
-  const list = document.querySelector(".list");
-  const body = document.querySelector("body");
-  burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    list.classList.toggle("active");
-    body.classList.toggle("lock");
-  });
-})();
-document.addEventListener("touchstart", (e) => {
-  let target = e.target;
-  let its_list = target == list || list.contains(target);
-  let its_burger = target == burger;
-  let list_is_active = list.classList.contains("active");
-
-  if (!its_list && !its_burger && list_is_active) {
-    burger.classList.toggle("active");
-    list.classList.toggle("active");
-    body.classList.toggle("lock");
-  }
-});
-//////////////Buger//////////
